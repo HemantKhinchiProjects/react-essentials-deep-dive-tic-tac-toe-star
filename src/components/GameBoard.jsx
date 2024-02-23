35f0e3ae24be8fa6f8f31fe482a2e414414f5042
Hemant Khinchi <99024438+HemantKhinchiProjects@users.noreply.github.com>
2024-02-23 14:26:09 +0000

    feat: Added useState hook to manage game board state and fixed rendering issues in handleSelectSquare function

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,17 +1,33 @@
+import { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
   [null, null, null],
 ];
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+  const handleSelectSquare = (rowIndex, cellIndex, playerSymbol) => {
+    setGameBoard((previousGameBoard) => {
+      const newGameBoard = [...previousGameBoard.map((row) => [...row])];
+      newGameBoard[rowIndex][cellIndex] = playerSymbol;
+      return newGameBoard;
+    });
+  };
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, cellIndex) => (
               <li key={cellIndex}>
-                <button>{playerSymbol}</button>
+                <button
+                  onClick={() =>
+                    handleSelectSquare(rowIndex, cellIndex, playerSymbol)
+                  }
+                >
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
